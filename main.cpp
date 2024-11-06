fe0ecd6a86f4fde2e0e500e8348dd42dc4f61847
Thbop <75861321+thbop@users.noreply.github.com>
2024-11-06 00:45:30 -0500

    Show normals

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@
 #include "color.h"
 #include "ray.h"
 
-bool hitSphere( const point3& center, double radius, const ray& r ) {
+double hitSphere( const point3& center, double radius, const ray& r ) {
     vec3 oc = center - r.o;
     auto a = dot(r.dir, r.dir);
     auto b = -2.0 * dot(r.dir, oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    
+    if ( discriminant < 0 ) return -1.0;
+    else                    return (-b - std::sqrt(discriminant)) / (2.0*a); // Solve for t
+    
 }
 
 color rayColor(const ray& r) {
-    if ( hitSphere( point3(0, 0, -1), 0.5, r ) )
-        return color(1, 0, 0);
+    auto t = hitSphere( point3(0, 0, -1), 0.5, r );
+    if ( t > 0.0 ) {
+        vec3 N = normalize(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
 
     vec3 dirNormal = normalize(r.dir);
     auto a = 0.5*(dirNormal.y() + 1.0);
